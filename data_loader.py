b6e50522cb52b996bf2a1c657e6204cb967b2404
faiazrahman <faiaz.rahman@yale.edu>
2020-09-20 13:48:51 -0400

    Read languages

--- data_loader.py
+++ data_loader.py
@@ -25,3 +25,33 @@ def normalize_string(s):
     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
     return s
 
+def read_languages(language1, language2, reverse=False):
+    """
+    Reads the data for translation FROM language1 TO language2
+    by default; reads FROM language2 TO language1 if reverse=True
+
+    Params
+        language1   (str) 3-letter abbreviation of language
+        language2   (str) 3-letter abbreviation of language
+        reverse     Specifies language2 -> language1 translation
+
+    Returns
+        input_language  (Language)
+        output_language (Language)
+        pairs           (list)
+    """
+    
+    lines = open(PATH_TO_DATA + '%s-%s.txt' % (language1, language2),
+                 encoding='utf-8').read().strip().split('\n')
+    
+    pairs = [[normalize_string(s) for s in l.split('\t')] for l in lines]
+
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_language = Language(language2)
+        output_language = Language(language1)
+    else:
+        input_language = Language(language1)
+        output_language = Language(language2)
+    
+    return input_language, output_language, pairs
