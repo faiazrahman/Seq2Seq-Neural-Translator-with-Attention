26ed67b941c2c03743409c7c149c9f28156731b1
faiazrahman <faiaz.rahman@yale.edu>
2020-09-21 23:14:59 -0400

    Decoder RNN architecture with ReLU in forward prop and softmax out

--- network.py
+++ network.py
@@ -11,17 +11,54 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class EncoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
+        """
+        Initializes encoder RNN, which includes a Gated Recurrent Unit
+        """
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
-
         self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
 
     def forward(self, input, hidden):
+        """
+        Forward propagation
+        """
         embedded = self.embedding(input).view(1, 1, -1)
         output = embedded
         output, hidden = self.gru(output, hidden)
         return output, hidden
 
     def initHidden(self):
+        """
+        Initializes hidden state
+        """
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        """
+        Initializes decoder RNN, which applies softmax to the GRU
+        output to get the final output, i.e. the translation
+        """
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        """
+        Forward propagation, which uses ReLU
+        """
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        """
+        Initializes hidden state
+        """
         return torch.zeros(1, 1, self.hidden_size, device=device)
